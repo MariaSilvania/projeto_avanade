ee558614140c7eb1da81c20c5bca1523504f6b6c
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-11-08 12:05:12 -0200

    Login configurado

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCV0cGzCoyU4_ObgTEfJ3ZQaRCKolT01MI",
+  authDomain: "projetoavanade.firebaseapp.com",
+  databaseURL: "https://projetoavanade.firebaseio.com",
+  projectId: "projetoavanade",
+  storageBucket: "projetoavanade.appspot.com",
+  messagingSenderId: "18157152138"
+};
+firebase.initializeApp(config);
