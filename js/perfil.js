6f571f6f53edd5736777b58c54908d0a308837d5
Alyne Martins <martins.alyne@gmail.com>
2018-11-09 04:47:11 -0200

    limpeza na home, ajustes de link

--- js/perfil.js
+++ js/perfil.js
@@ -1,19 +1,7 @@
 var database = firebase.database();
 var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
-$(document).ready(function () {
-	//	pegando o Nome do usuário
-	database.ref("user/" + USER_ID).once("value")
-		.then(function (snapshot) {
-			var userInfo = snapshot.val();
-			$(".user-name").text(userInfo.name);
-		})
-	//  Pegando usuários do firebase
-	// database.ref("user").once("value")
-	// 	.then(function (snapshot) {
-	// 		snapshot.forEach(function (childSnapshot) {
-	// 			var childKey = childSnapshot.key;
-	// 			var childData = childSnapshot.val();
-	// 			createUsers(childData.name, childKey);
-	// 	});
-	// })
-});
+
+function perfil() {
+	window.location = "perfil.html?id=" + USER_ID;
+	console.log(USER_ID);
+}
