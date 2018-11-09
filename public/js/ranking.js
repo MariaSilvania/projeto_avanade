9943fec98eb742fde7717a3070ac658b616e1f8c
Alyne Martins <martins.alyne@gmail.com>
2018-11-09 13:10:07 -0200

    ranking finalizado

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -1,6 +1,22 @@
+var database = firebase.database();
 var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
-function perfil() {
-  window.location = "perfil.html?id=" + USER_ID;
-  console.log(USER_ID);
+
+$(document).ready(function () {
+	database.ref("user").once("value").then(function (snapshot) {
+			snapshot.forEach(function (childSnapshot) {
+				var childKey = childSnapshot.key;
+				var childData = childSnapshot.val();
+				createRanking(childData.name, childKey, childData.ranking);
+			});
+		})
+})
+
+function createRanking(name, key, ranking) {
+	$(".ranking-info").append(`
+		<div class="box">
+			<h4>${name}</h4>
+			<h5 class="ranking"><img src="img/medal.png" width="25px">${ranking}</h5>
+		</div>
+		`);
 }
